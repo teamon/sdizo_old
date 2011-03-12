47a7d0cabca57d4c9f7965e93d338e47f438e48c
JW <jacek@jacek-PC.(none)>
2011-03-12 20:16:49 +0100

    Add Prim.cpp

---
+++ Projekt1/Prim/Prim.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Node.h"
+#include "../GMatrix.h"
+
+using namespace std;
+using namespace prim;
+
+int main(int argc, char const * argv[]){
+	cout << "Prim\n"; 
+	return 0;
+}
+
+
+
+ 
