5097eda30d2287d935657b8ee67afa71182ef3e4
jacbar <me@jacbar.pl>
2011-03-13 00:08:19 +0100

    dupa

--- Projekt1/main.cpp
+++ Projekt1/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main (int argc, char const *argv[]){
     cout << "Projekt 1" << endl;
     
-    GMatrix matrix = GMatrix(argv[1]);
+    GList list = GList(argv[1]);
     
-    PrimMatrix(matrix);
+    
 
     return 0;
 }
