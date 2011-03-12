e30798003aa09e98486902c61f0219d767eb24b2
jacbar <me@jacbar.pl>
2011-03-13 00:46:01 +0100

    change in main

--- Projekt1/main.cpp
+++ Projekt1/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <list>
 
 #include "Prim/Prim.h"
+
 
 using namespace std;
 
 int main (int argc, char const *argv[]){
     cout << "Projekt 1" << endl;
     
-    GList list = GList(argv[1]);
+    GList glist = GList(argv[1]);
     
+    list<Edge > * tab = glist.getTab();
+    
+    int n = glist.getN();
+    
+    for(int i =0 ; i<n; i++){
+		for(list<Edge>::iterator it = tab[i].begin(); it != tab[i].end(); it++){
+			cout << i << " " <<it->peak << " " << it->weight <<endl; 
+		}
+	}
     
 
     return 0;
