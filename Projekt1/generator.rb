3889859b50b6747f6f9e4d534f65d4842334c90e
Tymon Tobolski <i@teamon.eu>
2011-03-13 00:01:22 +0100

    Fixed list generator

--- Projekt1/generator.rb
+++ Projekt1/generator.rb
@@ -9,25 +9,24 @@ N = ARGV[1].to_i
 P = ARGV[2].to_i
 W = 10
 
+ar = N.times.map {|i| 
+   N.times.map { 0 }
+}
+
+(N*P).times.each {
+  x = rand(N)
+  y = rand(N)
+  
+  next if x == y
+  
+  w = rand(W)+1
+  ar[x][y] = ar[y][x] = w
+}
+
 case ARGV[0]
 when "matrix"
   File.open("data/matrix_#{N}_#{P}.txt", "w") do |file|
     file.puts N
-
-    ar = N.times.map {|i| 
-       N.times.map { 0 }
-    }
-
-    (N*P).times.each {
-      x = rand(N)
-      y = rand(N)
-      
-      next if x == y
-      
-      w = rand(W)+1
-      ar[x][y] = ar[y][x] = w
-    }
-    
 
     N.times do |i|
       N.times do |j|
@@ -42,9 +41,13 @@ when "list"
   all = (0..N-1).to_a
   File.open("data/list_#{N}_#{P}.txt", "w") do |file|
     file.puts N
+    
     N.times do |i|
-      x = all.sample((rand(10) < P) ? (rand(N)+1) : 0).map {|e| [e, rand(W) + 1] }
-      file.puts ([x.size] + x).flatten.join(" ")
+      (i..N-1).each do |j|
+        if ar[i][j] > 0
+          file.puts "#{i} #{j} #{ar[i][j]}"
+        end
+      end
     end
   end
 end
