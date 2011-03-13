c770309acb32bec70437c573826d548dff9c9964
Tymon Tobolski <i@teamon.eu>
2011-03-13 05:07:53 +0100

    Kruskal init

---
+++ Projekt1/Kruskal/Kruskal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Kruskal.h"
+#include <set>
+#include <stdio.h>
+
+using namespace std;
+// using namespace kruskal;
+
+void KruskalMatrix(GMatrix &matrix){
+        
+}
+
+void KruskalList(GList &list){
+    
+}
---
+++ Projekt1/Kruskal/Kruskal.h
@@ -0,0 +1,11 @@
+#ifndef __KRUSKAL_H_
+#define __KRUSKAL_H_
+
+#include "../GMatrix.h"
+#include "../GList.h"
+
+void KruskalMatrix(GMatrix &matrix);
+void KruskalList(GList &list);
+// void printNodes(prim::Node * nodes, int N);
+
+#endif
