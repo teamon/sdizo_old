13d7d732808103b9e9d652a9aa4d122c01fa1af8
Tymon Tobolski <i@teamon.eu>
2011-03-13 05:19:24 +0100

    Typo fixes

--- Projekt1/Edge.h
+++ Projekt1/Edge.h
@@ -1,6 +1,11 @@
- class Edge {
-	public :
-		int peak, weight;
-		Edge(int p, int w) : peak(p), weight(w){}
-		Edge() : peak(0), weight(0){}
- };
+#ifndef __EDGE_H_
+#define __EDGE_H_
+
+class Edge {
+public :
+    int peak, weight;
+    Edge(int p, int w) : peak(p), weight(w){}
+    Edge() : peak(0), weight(0){}
+};
+
+#endif
--- Projekt1/Kruskal/Kruskal.cpp
+++ Projekt1/Kruskal/Kruskal.cpp
@@ -10,12 +10,12 @@ using namespace std;
 void KruskalMatrix(GMatrix &matrix){
 	int ** tab = matrix.getTab();
 	int N = matrix.getN();
-    list<Edge> * glist = new list<Edge>[n];
+    list<Edge> * glist = new list<Edge>[N];
 	for(int i=0; i<N; i++){
 		for(int j=i+1; j < N; j++){
 			if(tab[i][j] != 0){
-				glit[i].push_back(Edge(j,tab[i][j]));
-				glit[j].push_back(Edge(i,tab[i][j]));
+				glist[i].push_back(Edge(j,tab[i][j]));
+				glist[j].push_back(Edge(i,tab[i][j]));
 			}
 		}
 	}
